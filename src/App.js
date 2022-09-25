8227ab2554f1193be04f0974004f7c4fbef9e599
haroldaqu <haroldgaquino@gmail.com>
2022-09-25 11:32:04 -0700

    Footer Component
    1. create footer component skeleton

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Navbar from './components/Navbar';
 import Banner from './components/Banner';
 import Skills from './components/Skills';
 import Projects from "./components/Projects";
+import Footer from "./components/Footer";
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
       <Banner />
       <Skills />
       <Projects />
+      <Footer />
     </div>
   );
 }
---
+++ src/components/Footer.js
@@ -0,0 +1,9 @@
+const Footer = () => {
+    return ( 
+        <footer className="container-xxl" id="footer" >
+            <p >Harold Aquino, 2016</p>
+        </footer>
+     );
+}
+ 
+export default Footer;
