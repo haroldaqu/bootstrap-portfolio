5b6aab9bacafe7640ae9fb4990f513da0ab7e5df
haroldaqu <haroldgaquino@gmail.com>
2022-10-01 09:03:30 -0700

    Skills and Skill Card Component size
    1. change size of skills and skills card

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -3,14 +3,14 @@ import skillsData from "../data/skillsData";
 
 const Skills = () => {
     return ( 
-        <section className="container-xxl" id="skills">
-            <div className="w-50" id="skills-header" >
-                <h1 id="headers" >SKILLS</h1>
-                <p> ... and technologies </p>
+        <section className="container-xxl text-center" id="skills">
+            <div className="w-100 h-25" id="skills-header" >
+                <h1 >SKILLS</h1>
+                <p> and technologies </p>
                 {/* <h2 className="border-bottom border-info" >Skills</h2>
                 <p>Technologies I've learned</p> */}
             </div>
-            <div className="w-50 row row-cols-1 row-cols-md-5 g-4" id="skills-group">
+            <div className="w-75 h-75 row row-cols-2 row-cols-md-3 g-4" id="skills-group">
                 {
                     skillsData.map((skill, i) => <div className="col"><SkillsCard key={i} skill={skill}/></div>)
                 }
